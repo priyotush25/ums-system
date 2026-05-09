3fd59a61cbc4231fe8d7f24ddba25e1cfc749417
priyotushpaul40@gmail.com <priyotushpaul40@gmail.com>
2026-05-09 10:06:43 -0700

    project setup

---
+++ frontend/src/router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello</div>,
+  },
+
+  
+]);
+
+export default Router;
